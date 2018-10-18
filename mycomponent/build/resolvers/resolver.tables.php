992fefaeec21f15aa6e9c89340f612bb866375d1
ArtProg <aprog-art@mail.ru>
2018-10-18 23:06:21 +0300

    Updated tables resolver

--- mycomponent/build/resolvers/resolver.tables.php
+++ mycomponent/build/resolvers/resolver.tables.php
@@ -5,19 +5,23 @@
  */
 
 if ($object->xpdo) {
+    $modx =& $object->xpdo;
+
+    //TODO put your package name here
+    $packageName = 'mycomponent';
+    $modelPath = $modx->getOption($packageName.'.core_path',null,$modx->getOption('core_path').'components/'.$packageName.'/').'model/';
+    $modx->addPackage($packageName,$modelPath);
+    $manager = $modx->getManager();
+
     switch ($options[xPDOTransport::PACKAGE_ACTION]) {
         case xPDOTransport::ACTION_INSTALL:
-            $modx =& $object->xpdo;
-
-            //TODO put your package name here
-            $packageName = 'mycomponent';
-            $modelPath = $modx->getOption($packageName.'.core_path',null,$modx->getOption('core_path').'components/'.$packageName.'/').'model/';
-            $modx->addPackage($packageName,$modelPath);
-            $manager = $modx->getManager();
             //TODO add your database related custom objects
             //$manager->createObjectContainer('myObjectClass');
             break;
         case xPDOTransport::ACTION_UPGRADE:
+            break;
+        case xPDOTransport::ACTION_UNINSTALL:
+            //$manager->removeObjectContainer('myObjectClass');
             break;
     }
 }
